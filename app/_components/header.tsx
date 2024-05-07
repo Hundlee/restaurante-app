844258c77c310fe12328a25758262da969a0b5a6
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-07 18:17:54 -0300

    style: switch width logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,8 +4,8 @@ import { MenuIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="Logo" width={60} height={60} />
+    <header className="flex items-center justify-between px-5 pt-6">
+      <Image src="/logo.png" alt="Logo" width={60} height={80} />
       <Button
         variant="outline"
         size="icon"
