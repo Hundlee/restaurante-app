89de4bee9306caa4a20f169025a105f95a47762f
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-11 13:10:42 -0300

    feat: Adds the next-link around the logo, to redirect to the home page on click

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="Logo" width={60} height={80} />
+      <Link href="/">
+        <Image src="/logo.png" alt="Logo" width={60} height={80} />
+      </Link>
       <Button
         variant="outline"
         size="icon"
