fdfb2d53b1e3703e5ddb5d86a0d6c6333577acd5
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-11 16:46:04 -0300

    feat: add next-link to redirect to the see more page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -44,14 +44,17 @@ export default async function Home() {
       </div>
 
       <div className="space-y-4 pl-5 pt-6">
-        <div className="flex items-center justify-between ">
+        <div className="flex items-center justify-between pr-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
           <Button
             variant="ghost"
             className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
           >
-            Ver Todos
-            <ChevronRightIcon size={16} />
+            <Link href="/products/recommended">
+              Ver Todos
+              <ChevronRightIcon size={16} />
+            </Link>
           </Button>
         </div>
         <ProductList products={products} />
@@ -65,17 +68,18 @@ export default async function Home() {
       </div>
 
       <div className="space-y-4 py-6 pl-5">
-        <div className="flex items-center justify-between ">
+        <div className="flex items-center justify-between pr-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
-          <Link href="/restaurants/recommended">
-            <Button
-              variant="ghost"
-              className="h-fit p-0 text-primary hover:bg-transparent"
-            >
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
+          >
+            <Link href="/restaurants/recommended">
               Ver Todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
         <RestauranteList />
       </div>
