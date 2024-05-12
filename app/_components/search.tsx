40c96323cf3b0f5e030d6ac2a211be5e63baa3c6
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-12 17:58:54 -0300

    feat: add search function in search component, for return search result

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,18 +1,39 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearchSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    router.push(`/restaurants?search=${search}`);
+  };
+
   return (
-    <div className="flex gap-2 ">
+    <form className="flex gap-2" onSubmit={handleSearchSubmit}>
       <Input
         placeholder="Buscar restaurantes"
         className="border-none bg-gray-200"
+        onChange={handleChange}
+        value={search}
       />
       <Button size="icon" className="h-10 w-12">
         <SearchIcon size={20} />
       </Button>
-    </div>
+    </form>
   );
 };
 
