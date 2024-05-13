c1e617101386365c798118e1306d2194e9fc5a77
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-13 17:24:01 -0300

    feat: add information session showing the discount value, total value, delivery value and base value

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -1,9 +1,14 @@
 import { useContext } from "react";
 import { CartContext } from "../_context/cart";
 import CartItem from "./cart-item";
+import { Card, CardContent } from "./ui/card";
+import { formatCurrency } from "../_helpers/price";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const Cart = () => {
-  const { products } = useContext(CartContext);
+  const { products, totalDiscount, totalPrice, subTotalPrice } =
+    useContext(CartContext);
 
   return (
     <div className="py-5">
@@ -12,6 +17,49 @@ const Cart = () => {
           <CartItem cartProducts={product} key={product.id} />
         ))}
       </div>
+
+      <div className="mt-6">
+        <Card>
+          <CardContent className="space-y-2 p-5">
+            <div className="flex items-center justify-between text-xs">
+              <span className="text-muted-foreground">Subtotal</span>
+              <span>{formatCurrency(subTotalPrice)}</span>
+            </div>
+
+            <Separator />
+
+            <div className="flex items-center justify-between text-xs">
+              <span className="text-muted-foreground">Total de Descontos</span>
+              <span>- {formatCurrency(totalDiscount)}</span>
+            </div>
+
+            <Separator />
+
+            <div className="flex items-center justify-between text-xs">
+              <span className="text-muted-foreground">Entrega</span>
+
+              {Number(products[0].restaurant.deliveryFee) === 0 ? (
+                <span className="uppercase text-primary">Grátis</span>
+              ) : (
+                formatCurrency(Number(products[0].restaurant.deliveryFee))
+              )}
+            </div>
+
+            <Separator />
+
+            <div className="flex items-center justify-between text-xs font-semibold ">
+              <span>Total</span>
+              <span>
+                {formatCurrency(
+                  totalPrice + Number(products[0].restaurant.deliveryFee),
+                )}
+              </span>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+
+      <Button className="mt-6 w-full">Finalizar Pedido</Button>
     </div>
   );
 };
