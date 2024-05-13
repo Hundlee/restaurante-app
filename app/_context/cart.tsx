2d586d7955e407f4484bddf8cafa893ce3c0c510
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-13 17:22:51 -0300

    feat: Updates shopping cart context with detailed price calculations
    - Updates the CartProduct type definition to include additional information about the product, such as the restaurant's deliveryFee.
    - Adds new fields to the shopping cart context, including subTotalPrice, totalPrice, and totalDiscount, to provide detailed information about the prices of products in the cart.
    - Uses the useMemo function to calculate prices efficiently and avoid repeated calculations.

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { Product } from "@prisma/client";
-import { ReactNode, createContext, useState } from "react";
+import { Prisma, Product } from "@prisma/client";
+import { ReactNode, createContext, useMemo, useState } from "react";
+import { calculateProductTotalPrice } from "../_helpers/price";
 
-export interface CartProduct extends Product {
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          deliveryFee: true;
+        };
+      };
+    };
+  }> {
   quantity: number;
 }
 
 interface ICardContext {
   products: CartProduct[];
+  subTotalPrice: number;
+  totalPrice: number;
+  totalDiscount: number;
   addProductToCart: (product: Product, quantity: number) => void;
   decreaseProductQuantity: (productId: string) => void;
   increaseProductQuantity: (productId: string) => void;
@@ -17,6 +30,9 @@ interface ICardContext {
 
 export const CartContext = createContext<ICardContext>({
   products: [],
+  subTotalPrice: 0,
+  totalPrice: 0,
+  totalDiscount: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
@@ -25,6 +41,20 @@ export const CartContext = createContext<ICardContext>({
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subTotalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscount = subTotalPrice - totalPrice;
 
   const decreaseProductQuantity = (productId: string) => {
     return setProducts((prev) =>
@@ -92,6 +122,9 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     <CartContext.Provider
       value={{
         products,
+        subTotalPrice,
+        totalPrice,
+        totalDiscount,
         addProductToCart,
         decreaseProductQuantity,
         increaseProductQuantity,
