386c8283c0b6ee300c7657ba331ac8718b9e65cf
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-13 14:57:58 -0300

    feat: created CardContext, with functions to add, remove and change product quantity

---
+++ app/_context/cart.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import { ReactNode, createContext, useState } from "react";
+
+export interface CartProduct extends Product {
+  quantity: number;
+}
+
+interface ICardContext {
+  products: CartProduct[];
+  addProductToCart: (product: Product, quantity: number) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+  removeProductToCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICardContext>({
+  products: [],
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductToCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          if (cartProduct.quantity === 1) {
+            return cartProduct;
+          }
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    return setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const addProductToCart = (product: Product, quantity: number) => {
+    const isProductAlreadyOnCart = products.some(
+      (cartProducts) => cartProducts.id === product.id,
+    );
+
+    if (isProductAlreadyOnCart) {
+      return setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+    }
+
+    setProducts((prev) => [...prev, { ...product, quantity: quantity }]);
+  };
+
+  const removeProductToCart = (productId: string) => {
+    return setProducts((prev) =>
+      prev.filter((product) => product.id !== productId),
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductToCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
