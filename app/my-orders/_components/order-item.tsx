e73be4bc7d83fb039e16df784230826e53e51d93
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-17 18:20:12 -0300

    feat: created order-item component

---
+++ app/my-orders/_components/order-item.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { Avatar, AvatarImage } from "@/app/_components/ui/avatar";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Separator } from "@/app/_components/ui/separator";
+import { formatCurrency } from "@/app/_helpers/price";
+import { OrderStatus, Prisma } from "@prisma/client";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+
+interface OrderItemProps {
+  order: Prisma.OrderGetPayload<{
+    include: {
+      restaurant: true;
+      products: {
+        include: {
+          product: true;
+        };
+      };
+    };
+  }>;
+}
+
+const getOrderStatusLabel = (status: OrderStatus) => {
+  switch (status) {
+    case "CANCELED":
+      return "Cancelado";
+    case "CONFIRMED":
+      return "Confirmado";
+    case "PREPARING":
+      return "Preparando";
+    case "DELIVERING":
+      return "Em Transporte";
+    case "COMPLETED":
+      return "Entregue";
+  }
+};
+
+const OrderItem = ({ order }: OrderItemProps) => {
+  return (
+    <Card>
+      <CardContent className="p-5">
+        <div className="w-fit rounded-full bg-[#EEEEEE] px-2 py-1 text-muted-foreground">
+          <span className="block text-xs font-semibold">
+            {getOrderStatusLabel(order.status)}
+          </span>
+        </div>
+
+        <div className="flex items-center justify-between pt-3">
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage
+                src={order.restaurant.imageUrl}
+                alt={order.restaurant.name}
+              />
+            </Avatar>
+            <span className="block text-sm font-semibold">
+              {order.restaurant.name}
+            </span>
+          </div>
+
+          <Button size="icon" variant="ghost" className="h-5 w-5">
+            <Link href={`/restaurants/${order.restaurant.id}`}>
+              <ChevronRightIcon />
+            </Link>
+          </Button>
+        </div>
+
+        <div className="py-3">
+          <Separator />
+        </div>
+
+        <div className="space-y-1">
+          {order.products.map((product) => (
+            <div key={product.id} className="flex items-center gap-1">
+              <div className="flex h-5 w-5 items-center justify-center rounded-full bg-muted-foreground">
+                <span className="block text-xs text-white">
+                  {product.quantity}
+                </span>
+              </div>
+              <span className="text-xs text-muted-foreground">
+                {product.product.name}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div className="py-3">
+          <Separator />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <p className="text-sm">{formatCurrency(Number(order.totalPrice))}</p>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="text-xs font-semibold text-primary"
+          >
+            Adicionar à sacola
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default OrderItem;
